e99d59e9d3ea833b557abf3ac9dc60d58ba7ef90
MrBTTF <zhvladi@gmail.com>
2016-05-19 20:38:23 +0300

    Added splash screen and icon

--- app/src/main/java/com/mrbttf/translator2/Splash.java
+++ app/src/main/java/com/mrbttf/translator2/Splash.java
@@ -13,6 +13,7 @@ public class Splash extends Activity {
     /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 800;
 
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
